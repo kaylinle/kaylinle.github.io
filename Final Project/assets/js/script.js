29c0ccc9450a415f6f1d8d4acfa0556a209a0627
kaylinle <kaylinle@gmail.com>
2016-11-23 15:32:07 -0800

    First Push

---
+++ Final Project/assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+  // Sidebar
+
+  $('#sidebar-button').click(function() {
+    if ($("#sidebar-button").hasClass("button-active")) {
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+      $("body").removeClass("no-scroll");
+
+    }
+
+    else {
+      $("#sidebar-button").addClass("button-active");
+      $(".sidebar-container").addClass("sidebar-active");
+      $(".page-wrapper").addClass("wrapper-active");
+      setTimeout(function() {
+        $("body").addClass("no-scroll");
+      }, 300);
+    }
+
+  });
+
+   $(".page-wrapper").click(function(){
+    if ($(".page-wrapper").hasClass("wrapper-active")){
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+      $("body").removeClass("no-scroll");
+    }
+
+
+  }); 
+
+});
