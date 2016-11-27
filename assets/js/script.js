f022f002e70b14960d804d8ab97f59038bac5018
kaylinle <kaylinle@gmail.com>
2016-11-26 18:29:02 -0800

    seventh

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  // Sidebar
+
+  $('#sidebar-button').click(function() {
+    if ($("#sidebar-button").hasClass("button-active")) {
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+      $("body").removeClass("no-scroll");
+
+    }
+
+    else {
+      $("#sidebar-button").addClass("button-active");
+      $(".sidebar-container").addClass("sidebar-active");
+      $(".page-wrapper").addClass("wrapper-active");
+      setTimeout(function() {
+        $("body").addClass("no-scroll");
+      }, 300);
+    }
+
+  });
+
+   $(".page-wrapper").click(function(){
+    if ($(".page-wrapper").hasClass("wrapper-active")){
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+      $("body").removeClass("no-scroll");
+    }
+  });
+
+  // Lightbulb click
+  $("#light-icon").click(function(){
+      $("#profile-img").fadeIn(400);
+  });
+
+  // Contact
+  $("#link").click(function(){
+      $("#email").fadeIn(1000);
+      $("#phone").fadeIn(1000);
+  });
+
+});
