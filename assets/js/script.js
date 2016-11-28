0c99943aa0721d15ceb16eaa04b67e9454c69494
kaylinle <kaylinle@gmail.com>
2016-11-27 22:52:22 -0800

    almost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,15 +31,15 @@ $(document).ready(function() {
   });
 
    $(".sidebar-item").click(function(){
-     $("body").removeClass("no-scroll");
-     $( "html, body").animate({
-       scrollTop: $($.attr(this, "href:")).offset().top
-     }, 400);
-     $(this).delay(4000);
-       $("#sidebar-button").removeClass("button-active");
-       $(".sidebar-container").removeClass("sidebar-active");
-       $(".page-wrapper").removeClass("wrapper-active");
-    });
+    $("body").removeClass("no-scroll");
+    $("html, body").animate({
+      scrollTop: $($.attr(this, "href:")).offset().top
+    }, 400);
+    $(this).delay(8000);
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+  }); 
 
   // Lightbulb click
   $("#light-icon").click(function(){
